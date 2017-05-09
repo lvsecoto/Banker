75072061d6d83b9b2773d89d17389a5b29f9aaa9
YuanJunYao <785570262@qq.com>
2017-05-09 15:43:46 +0800

    feat: beautify balance text view on depositor list item
    The balance text view is  a little higher than ever.
    The color of banker balance text view is set to accent color.

--- app/src/main/java/com/yjy/banker/component/activity/baseActivity/DepositorListFragment.java
+++ app/src/main/java/com/yjy/banker/component/activity/baseActivity/DepositorListFragment.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ListFragment;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
@@ -275,6 +276,12 @@ public abstract class DepositorListFragment extends ListFragment {
 
             Integer balance = mBalanceList.get(accountID);
             holder.balanceTextView.setText(getBalanceString(balance));
+
+            if (accountID.getID() == 0) {
+                holder.balanceTextView.setBackgroundColor(
+                        ContextCompat.getColor(mActivity, R.color.colorAccent)
+                );
+            }
         }
 
         private String getBalanceString(Integer balance) {
